296d2a08d95bb557a01a8fed95c37bd7553b2a89
Borna Sila <bsila@riteh.hr>
2021-03-23 22:52:52 +0100

    initializing project

---
+++ Temp_control_mcu/main.c
@@ -0,0 +1,18 @@
+/*
+ * Temp_control_mcu.c
+ *
+ * Created: 23.3.2021. 22:44:18
+ * Author : Korisnik
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
